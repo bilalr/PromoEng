96279b594e38614f8e7ba8a509bf96738bfc29c4
bilalr <itsbilal@gmail.com>
2021-05-16 15:36:29 +0200

    add scenario A test case

--- src/BR.PromoEng/BR.PromoEng/Models/Promotion.cs
+++ src/BR.PromoEng/BR.PromoEng/Models/Promotion.cs
@@ -1,10 +1,18 @@
 ﻿namespace BR.PromoEng.Models
 {
+    /// <summary>
+    /// abstraction for promotion 
+    /// give a possiblity to extentions.
+    /// </summary>
     public abstract class Promotion
     {
         public decimal price { get; set; }
         public PromotionType PromotionType { get; set; }
     }
+    /// <summary>
+    /// Type of promotions. 
+    /// If new type come so need to add it here
+    /// </summary>
     public enum PromotionType
     {
         IndividualSKU = 0,
